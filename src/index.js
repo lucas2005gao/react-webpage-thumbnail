a963abff03832a1d316d00700000c4e88a38a528
Lucas Gao <lucas2005gao@gmail.com>
2021-09-23 01:06:57 +1200

    🚀 NEW RELEASE: prop passing implemented

--- src/index.js
+++ src/index.js
@@ -1,43 +1,58 @@
 import React from "react";
 
-const iframestyle = {
-  transform: "scale(0.15)",
-  transformOrigin: "0 0",
-  width: "1500px",
-  height: "1000px",
-  border: "0px",
-};
-
 function Thumbnail({
-  url,
-  iframeWidth,
-  iframeHeight,
-  width,
-  height,
-  interactive,
+  url = "https://canvas.ac.nz/",
+  iframeWidth = 1500,
+  iframeHeight = 1500,
+  width = 150,
+  height = 150,
+  interactive = false,
 }) {
+  const xscale = width / iframeWidth;
+  const yscale = height / iframeHeight;
+  const iframe = {
+    transform: `scaleX(${xscale}) scaleY(${yscale})`,
+    transformOrigin: "0 0",
+    width: `${iframeWidth}px`,
+    height: `${iframeHeight}px`,
+    border: "0px",
+  };
+
+  const iframeShade = {
+    position: "fixed",
+    width: "100%",
+    height: "100%",
+    maxWidth: `${width}px`,
+    maxHeight: `${height}px`,
+    zIndex: "10",
+  };
+
+  const iframeWrapper = {
+    maxWidth: `${width}px`,
+    maxHeight: `${height}px`,
+  };
+
+  const outerWrapper = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: "100%",
+  };
+
   return (
-    <div style={{ maxWidth: "300px", maxHeight: "160px" }}>
-      <div
-        style={{
-          position: "fixed",
-          width: "100%",
-          height: "100%",
-          maxWidth: "300px",
-          maxHeight: "160px",
-          zIndex: "10",
-        }}
-      >
-        a
+    <div style={outerWrapper}>
+      <div style={iframeWrapper}>
+        {!interactive && <div style={iframeShade}> </div>}
+        <iframe
+          tabIndex="-1"
+          style={iframe}
+          title="asdf"
+          src={url}
+          sandbox="allow-scripts allow-same-origin"
+          scrolling="no"
+        />
       </div>
-      <iframe
-        tabIndex="-1"
-        style={iframestyle}
-        title="asdf"
-        src="http://www.google.com/"
-        sandbox="allow-scripts allow-same-origin"
-        scrolling="no"
-      />
     </div>
   );
 }
