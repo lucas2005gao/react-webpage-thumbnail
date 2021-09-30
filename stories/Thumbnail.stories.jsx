ae549109f2f35d88052f35f848a2448d859f8634
Lucas Gao <lucas2005gao@gmail.com>
2021-10-01 01:19:09 +1300

    🚀 NEW RELEASE: 0.0.3 release including storybook

--- stories/Thumbnail.stories.jsx
+++ stories/Thumbnail.stories.jsx
@@ -9,7 +9,39 @@ export default {
 
 const Template = (args) => <Thumbnail {...args} />;
 
-export const LoggedIn = Template.bind({});
-LoggedIn.args = {
-  height: 500,
+// export const defaultThumbnail = Template.bind({});
+
+export const sizeDefinedThumbnail = Template.bind({});
+sizeDefinedThumbnail.args = {
+  width: 400,
+  height: 300,
 };
+
+export const iframeNotAllowedThumbnail = Template.bind({});
+iframeNotAllowedThumbnail.args = {
+  url: "https://www.google.com/",
+  width: 400,
+  height: 300,
+};
+
+// export const fitThumbnailInContainer = () => {
+//   return (
+//     <div style={{ width: 500, height: 400, background: "blue" }}>
+//       {Template.bind({})()}
+//     </div>
+//   );
+// };
+
+// export const fitSmallerThumbnailInContainer = () => {
+//   const thumbnail = Template.bind({});
+//   thumbnail.args = {
+//     url: "https://www.google.com/",
+//     width: 400,
+//     height: 300,
+//   };
+//   return (
+//     <div style={{ width: 500, height: 400, background: "blue" }}>
+//       {thumbnail()}
+//     </div>
+//   );
+// };
