0dfd4a64f50464e989cc0d4aee71aa1a283d3798
Lucas Gao <lucas2005gao@gmail.com>
2023-10-01 14:22:32 +1300

    Upgrade npm packages

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,5 +3,15 @@ module.exports = {
     "../stories/*.stories.mdx",
     "../stories/*.stories.@(js|jsx|ts|tsx)",
   ],
+
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
+  }
 };
